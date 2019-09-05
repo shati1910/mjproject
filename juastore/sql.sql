ca17c8eb1bdb2b75e176545fd9bcd132669d9f7f
shati1910 <51812933+shati1910@users.noreply.github.com>
2019-09-05 09:16:43 +0900

    Revert "Hayun"

--- juastore/sql.sql
+++
@@ -1,9 +0,0 @@
-create view product_inventory_view as select
-product.product_code as product_code,
-product.product_name as product_name,
-product.product_price as product_price,
-product.image as image,
-product.etc as etc,
-((select sum(inventory_amount) from inventory where inventory_state='in' and product.product_code=inventory.product_code)-
-(select sum(inventory_amount) from inventory where inventory_state='out' and product.product_code=inventory.product_code)) as inventory_amount
-from inventory left join product on product.product_code=inventory.product_code group by product_code;
